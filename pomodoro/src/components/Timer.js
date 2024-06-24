888ad243553187fef477e85389e1e9a284dd4489
Mahedere <mahederetebebe98@gmail.com>
2024-06-24 09:40:13 -0500

    Timer replaced

--- pomodoro/src/components/Timer.js
+++ pomodoro/src/components/Timer.js
@@ -1,12 +1,26 @@
- import { CircularProgressbar ,buildStyles} from "react-circular-progressbar"
- import 'react-circular-progressbar/dist/styles.css'
+import React from "react";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import 'react-circular-progressbar/dist/styles.css';
 
-function Timer (){
+const red = '#f54e4e';
+const green = '#4aec8c';
+
+function Timer() {
   return (
     <div>
-        <CircularProgressbar value={60} text={"60%"}/>
+      <CircularProgressbar
+        value={60}
+        text={"60%"}
+        styles={buildStyles({
+          rotation: 0.0,
+          strokeLinecap: 'butt',
+          textColor: '#fff',
+          pathColor: red,
+          trailColor: 'rgba(255,255,255,.2)',
+        })}
+      />
     </div>
-  )
+  );
 }
 
-export default Timer
+export default Timer;
