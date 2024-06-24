7e896ab21a3a0f04531165c4d9f6e4f8434b1f54
Mahedere <mahederetebebe98@gmail.com>
2024-06-24 16:21:41 -0500

    back button css completed

--- pomodoro/src/components/Settings.js
+++ pomodoro/src/components/Settings.js
@@ -3,27 +3,27 @@ import ReactSlider from 'react-slider';
 import SettingsContext from './SettingsContext';
 import BackButton from './BackButton';
 function Settings() {
-  const settingsInfo=useContext(SettingsContext);
+    const settingsInfo = useContext(SettingsContext);
     return (
         <div style={{ textAlign: 'left' }}>
 
             <label>Work:{settingsInfo.workMinutes}:00</label>
             <ReactSlider className={'slider'}
-            thumbClassName={'thumb'}
-            trackClassName={'track'}
-            value={settingsInfo.workMinutes}
-            onChange={newValue => settingsInfo.setWorkMinutes(newValue)}
-            min={1}
-            max={120}/>
+                thumbClassName={'thumb'}
+                trackClassName={'track'}
+                value={settingsInfo.workMinutes}
+                onChange={newValue => settingsInfo.setWorkMinutes(newValue)}
+                min={1}
+                max={120} />
             <label>break:{settingsInfo.breakMinutes}:00</label>
             <ReactSlider className={'slider green'}
-            thumbClassName={'thumb'}
-            trackClassName={'track'}
-            value={settingsInfo.breakMinutes}
-            onChange={newValue => settingsInfo.setBreakMinutes(newValue)}
-            min={1}
-            max={120}/>
-           <BackButton/>
+                thumbClassName={'thumb'}
+                trackClassName={'track'}
+                value={settingsInfo.breakMinutes}
+                onChange={newValue => settingsInfo.setBreakMinutes(newValue)}
+                min={1}
+                max={120} />
+            <div style={{textAlign:'center',marginTop:'20px'}}>     <BackButton /></div>
         </div>
     )
 }
